59adf0122fd3ef284821ac1387664f72562608e4
Lee <blamedrlee@outlook.com>
2018-12-17 18:21:04 +0800

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,150 @@
+#! /usr/bin/bash
+set -e
+
+######## hardware (default) ########
+# device
+device=-1
+
+######## dataset (default) ########
+# language: zh-en or en-zh
+s=de
+t=en
+
+# problems
+problems=iwslt14
+
+# used for specific data file
+data_dir=../data/data-bin
+
+# datatype=SPLIT, e.g. train, valid, test
+datatype=test
+
+######## parameters (default) ########
+batch_size=128
+beam_size=5
+lenpen=1
+# if use ensemble, set it
+ensemble=
+# dynamic options, e.g. change some default settings, '--quiet'
+other_options="--quiet --remove-bpe --no-progress-bar"
+
+######## models (default) ########
+# must exist
+tag=transformer
+# used for specific model file
+model_file=checkpoint_best.pt
+
+######## evaluation (default) ########
+# evaluation or just decoding
+is_eval=1
+# now available for multi-bleu.perl only
+eval_tool=../utils/scripts/multi-bleu.perl
+
+######## args (reset default) ########
+# use args to reset default settings
+while getopts a:b:c opt; do
+  case $opt in
+    a)
+      echo -e "\033[33mreset datatype from $datatype to $OPTARG by -a\033[0m"
+      datatype=$OPTARG
+      ;;
+    b)
+      echo -e "\033[33mreset model_file from $model_file to $OPTARG by -b\033[0m"
+      model_file=$OPTARG
+      ;;
+    c)
+      echo -e "\033[33mdisable evaluation by -d\033[0m"
+      is_eval=0
+      ;;
+    \?)
+      echo  -e "\033[31mInvalid option: -$OPTARG\033[0m"
+      ;;
+  esac
+done
+# if we want to set default value for a variable defined in the while loop, try the following code ("" required for string)
+# new_var=${new_var:-default}
+
+######## dataset (no reset) ########
+# language: zh-en or en-zh
+lang=${s}"-"${t}
+# dataset
+dataset=${problems}.tokenized.${lang}
+
+######## models (no reset) ########
+# used for specific model file
+output_dir=../checkpoints/${tag}
+
+######## evaluation (no reset) ########
+# used for specific output file
+if [ ${is_eval} -eq 1 ]; then
+  output_file=${output_dir}/trans.${t}
+else
+  output_file=
+fi
+# for single reference evaluation
+ref_file_bpe=${data_dir%%/data-bin*}/${dataset}/${datatype}.${t}
+ref_file=${output_dir}/${datatype}.${t}
+
+
+
+if [ -n "$ensemble" ]; then
+  # start ensemble
+  model_file=model.pt
+  echo -e "\033[34mensemble the last $ensemble checkpoints from $output_dir to $output_dir/$model_file\033[0m"
+  if [ -n "$output_dir/$model_file" ]; then
+    echo -e "\033[33mWarning: override $output_dir/$model_file\033[0m"
+  fi
+  PYTHONPATH=`pwd` python3 scripts/average_checkpoints.py \
+  --inputs ${output_dir} \
+  --num-epoch-checkpoints ${ensemble} \
+  --output ${output_dir}/${model_file}
+  if [ $? -ne 0 ]; then
+    exit -1
+  fi
+fi
+
+cmd="python3 -u generate.py
+$data_dir/$dataset
+--gen-subset $datatype
+--path $output_dir/$model_file
+--batch-size $batch_size
+--beam $beam_size
+--lenpen $lenpen"
+if [ -n "$output_file" ]; then
+  cmd=${cmd}" --output-file "${output_file}
+fi
+if [ -n "$other_options" ]; then
+  cmd=${cmd}" "${other_options}
+fi
+
+# start decoding
+echo -e "\033[34mrun command: "${cmd}"\033[0m"
+CUDA_VISIBLE_DEVICES=${device} PYTHONPATH=`pwd` $cmd | tee $output_dir/generate.log
+
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
+if [ ${is_eval} -eq 1 ]
+then
+  # remove bpe
+  cp ${ref_file_bpe} ${ref_file}
+  sed -i s'/@@ //g' ${ref_file}
+  sed -i s'/@@ //g' ${output_file}
+
+  # check eval_tool
+  if [ -n "$eval_tool" ]; then
+	echo -e "\033[33musing evaluation tool: $eval_tool\033[0m"
+  else
+	echo -e "\033[31m$eval_tool does not exist!\033[0m"
+    exit -1
+  fi
+
+  # run eval_tool
+  echo -e "\033[34mrun command: perl $eval_tool $ref_file < $output_file\033[0m"
+  perl $eval_tool $ref_file < $output_file
+
+  rm $ref_file
+
+fi
+
---
+++ train.sh
@@ -0,0 +1,90 @@
+#! /usr/bin/bash
+set -e 
+
+######## hardware ########
+# devices
+devices=1
+
+######## dataset ########
+# language: zh-en or en-zh
+s=de
+t=en
+lang=${s}"-"${t}
+# dataset
+dataset=iwslt14.tokenized.${lang}
+
+######## parameters ########
+# which model
+model=transformer
+# which hparams 
+param=${model}"_iwslt_"${s}"_"${t}
+# defualt is 103k. About 10 epochs for 700w CWMT
+max_update=50000
+# dynamic hparams, e.g. change the batch size without the register in code, other_hparams='batch_size=2048'
+other_hparams=
+
+######## required ########
+# tag is the name of your experiments
+tag=transformer
+
+
+
+# automatically set worker_gpu according to $devices
+worker_gpu=`echo "$devices" | awk '{split($0,arr,",");print length(arr)}'`
+# dir of training data
+data_dir=../data/data-bin
+# dir of models
+output_dir=../checkpoints/${tag}
+
+if [ ! -d "$output_dir" ]; then
+  mkdir -p ${output_dir}
+else
+  echo -e "\033[31m$output_dir exists!\033[0m"
+  exit -1
+fi
+
+if [ ! -d "$data_dir/$dataset" ]; then
+  # start preprocessing
+  echo -e "\033[34mpreprocess from ${data_dir%%/data-bin*}/$dataset to $data_dir/$dataset\033[0m"
+  python3 -u preprocess.py \
+  --source-lang ${s} \
+  --target-lang ${t} \
+  --trainpref ${data_dir%%/data-bin*}/${dataset}/train \
+  --validpref ${data_dir%%/data-bin*}/${dataset}/valid \
+  --testpref ${data_dir%%/data-bin*}/${dataset}/test \
+  --destdir ${data_dir}/${dataset}
+fi
+
+adam_betas="'(0.9, 0.98)'"
+
+cmd="python3 -u train.py
+$data_dir/$dataset
+-a $param
+-s $s
+-t $t
+--no-progress-bar
+--optimizer adam
+--lr 0.0005
+--label-smoothing 0.1
+--dropout 0.3
+--max-tokens 4000
+--min-lr 1e-09
+--lr-scheduler inverse_sqrt
+--weight-decay 0.0001
+--criterion label_smoothed_cross_entropy
+--max-update $max_update
+--warmup-updates 4000
+--warmup-init-lr 1e-07
+--save-dir $output_dir"
+cmd=${cmd}" --adam-betas "${adam_betas}
+if [ -n "$other_hparams" ]; then
+  cmd=${cmd}" "${other_hparams}
+fi
+
+echo -e "\033[34mrun command: "${cmd}"\033[0m"
+# start training
+cmd="CUDA_VISIBLE_DEVICES=$devices nohup "${cmd}" > $output_dir/train.log 2>&1 &"
+eval $cmd
+
+# monitor training log
+tail -f ${output_dir}/train.log
