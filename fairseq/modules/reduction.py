30f2696b7cc3404600de5fa2156b1c118d1752c2
Lee <blamedrlee@outlook.com>
2019-08-27 11:35:20 +0800

    Add Reduction variants

--- fairseq/modules/reduction.py
+++ fairseq/modules/reduction.py
@@ -54,6 +54,36 @@ class Reduction(nn.Module):
         else:
             return x
 
+    @register_to('max', _VALID_REDUCTION)
+    def max(self, args):
+        """
+        Reduce the source dimension based on their maximum
+        :param args: Namespace
+        :return: Callable
+        """
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            # T x S x B x C
+            if mask is not None:
+                x = x.masked_fill(
+                    mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1),
+                    float('-inf'),
+                )
+            # T x B x C
+            x = x.max(dim=1)[0]
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
     @register_to('attn', _VALID_REDUCTION)
     def attn(self, args):
         """
@@ -84,8 +114,257 @@ class Reduction(nn.Module):
                     float('-inf'),
                 )
             prob = F.softmax(weights, dim=1)
-            assert torch.isnan(prob).byte().any() == 0
             x = torch.sum(prob * x, dim=1)
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
+    @register_to('attn-v2', _VALID_REDUCTION)
+    def attn_v2(self, args):
+        """
+        x * w1 + b1 -> k: embed
+        x * w2 + b2 -> v
+        softmax(k) * v -> y
+        :param args: Namespace
+        :return: Callable
+        """
+        self.in_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim * 2)
+        nn.init.xavier_uniform_(self.in_proj.weight)
+        nn.init.constant_(self.in_proj.bias, 0.)
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            # T x S x B x 2C
+            k, v = self.in_proj(x).chunk(2, dim=-1)
+            if mask is not None:
+                mask = mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1)
+                k = k.masked_fill(
+                    mask,
+                    float('-inf'),
+                )
+            prob = F.softmax(k, dim=1)
+            x = torch.sum(prob * v, dim=1)
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
+    @register_to('attn-v3', _VALID_REDUCTION)
+    def attn_v3(self, args):
+        """
+        x * w1 + b1 -> k: embed
+        x * w2 + b2 -> v
+        softmax(k) * v -> y'
+        y' * w3 + b3 -> y
+        :param args: Namespace
+        :return: Callable
+        """
+        self.in_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim * 2)
+        nn.init.xavier_uniform_(self.in_proj.weight)
+        nn.init.constant_(self.in_proj.bias, 0.)
+        self.out_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim)
+        nn.init.xavier_uniform_(self.out_proj.weight)
+        nn.init.constant_(self.out_proj.bias, 0.)
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            # T x S x B x 2C
+            k, v = self.in_proj(x).chunk(2, dim=-1)
+            if mask is not None:
+                mask = mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1)
+                k = k.masked_fill(
+                    mask,
+                    float('-inf'),
+                )
+            prob = F.softmax(k, dim=1)
+            x = torch.sum(prob * v, dim=1)
+            x = self.out_proj(x)
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
+    @register_to('attn-v4', _VALID_REDUCTION)
+    def attn_v4(self, args):
+        """
+        x * w1 -> k: embed
+        softmax(k) * x -> y'
+        y' * w2 + b2 -> y
+        :param args: Namespace
+        :return: Callable
+        """
+        self.in_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim, bias=False)
+        nn.init.xavier_uniform_(self.in_proj.weight)
+        self.out_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim)
+        nn.init.xavier_uniform_(self.out_proj.weight)
+        nn.init.constant_(self.out_proj.bias, 0.)
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            # T x S x B x C
+            k = self.in_proj(x)
+            if mask is not None:
+                mask = mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1)
+                k = k.masked_fill(
+                    mask,
+                    float('-inf'),
+                )
+            prob = F.softmax(k, dim=1)
+            x = torch.sum(prob * x, dim=1)
+            x = self.out_proj(x)
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
+    @register_to('attn-v5', _VALID_REDUCTION)
+    def attn_v5(self, args):
+        """
+        x * w1 -> k: 1
+        softmax(k) * x -> y'
+        y' * w2 + b2 -> y
+        :param args: Namespace
+        :return: Callable
+        """
+        self.in_proj = nn.Linear(args.decoder_model_dim, 1, bias=False)
+        nn.init.xavier_uniform_(self.in_proj.weight)
+        self.out_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim)
+        nn.init.xavier_uniform_(self.out_proj.weight)
+        nn.init.constant_(self.out_proj.bias, 0.)
+        self.scaling = args.decoder_model_dim ** -0.5
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            # T x S x B x 1
+            attn_weights = self.in_proj(x) * self.scaling
+            if mask is not None:
+                mask = mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1)
+                attn_weights = attn_weights.masked_fill(
+                    mask,
+                    float('-inf'),
+                )
+            prob = F.softmax(attn_weights, dim=1)
+            x = torch.sum(prob * x, dim=1)
+            x = self.out_proj(x)
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
+    @register_to('attn-v6', _VALID_REDUCTION)
+    def attn_v6(self, args):
+        """
+        x * w1 -> k: 1
+        softmax(k) * x -> y
+        :param args: Namespace
+        :return: Callable
+        """
+        self.in_proj = nn.Linear(args.decoder_model_dim, 1, bias=False)
+        nn.init.xavier_uniform_(self.in_proj.weight)
+        self.scaling = args.decoder_model_dim ** -0.5
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            # T x S x B x 1
+            attn_weights = self.in_proj(x) * self.scaling
+            if mask is not None:
+                mask = mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1)
+                attn_weights = attn_weights.masked_fill(
+                    mask,
+                    float('-inf'),
+                )
+            prob = F.softmax(attn_weights, dim=1)
+            x = torch.sum(prob * x, dim=1)
+            x = self.maybe_layer_norm(self.layer_norm, x, after=True)
+            return x
+
+        return _forward
+
+    @register_to('attn-v7', _VALID_REDUCTION)
+    def attn_v7(self, args):
+        """
+        x * w1 -> k: head
+        softmax(k) * x -> y'
+        y' * w2 + b2 -> y
+        :param args: Namespace
+        :return: Callable
+        """
+        self.embed_dim = args.decoder_model_dim
+        self.num_heads = args.decoder_attention_heads
+        self.head_dim = self.embed_dim // self.num_heads
+        assert self.head_dim * self.num_heads == self.embed_dim, "embed_dim must be divisible by num_heads"
+
+        self.in_proj = nn.Linear(self.head_dim, self.num_heads, bias=False)
+        nn.init.xavier_uniform_(self.in_proj.weight)
+        self.out_proj = nn.Linear(args.decoder_model_dim, args.decoder_model_dim)
+        nn.init.xavier_uniform_(self.out_proj.weight)
+        nn.init.constant_(self.out_proj.bias, 0.)
+
+        self.scaling = self.head_dim ** -0.5
+
+        def _forward(x, mask, incremental_state=None):
+            """
+            Customized forward function
+            :param x: torch.FloatTensor, T x S x B x C
+            :param mask: torch.ByteTensor, B x S, masked elements indicated by 1
+            :param incremental_state: Dictionary
+            :return: torch.FloatTensor, T x B x C
+            """
+            x = self.maybe_layer_norm(self.layer_norm, x, before=True)
+            t, s, b, c = x.size()
+            # T x S x B x H x D
+            x = x.view(t, s, b, self.num_heads, self.head_dim)
+            # T x S x B x H x H
+            attn_weights = self.in_proj(x) * self.scaling
+            # T x S x B x H
+            attn_weights = torch.diagonal(attn_weights, dim1=3, dim2=4)
+            if mask is not None:
+                mask = mask.transpose(0, 1).unsqueeze(0).unsqueeze(-1)
+                attn_weights = attn_weights.masked_fill(
+                    mask,
+                    float('-inf'),
+                )
+            # T x S x B x H
+            prob = F.softmax(attn_weights, dim=1)
+            # T x B x H x D
+            x = torch.sum(prob.unsqueeze(-1) * x, dim=1).view(t, b, c)
+            x = self.out_proj(x)
             x = self.maybe_layer_norm(self.layer_norm, x, after=True)
             return x
 
@@ -101,7 +380,6 @@ class Reduction(nn.Module):
         :param args: Namespace
         :return: Callable
         """
-        self.dropout = args.dropout
         self.self_attn = SeparableAttention(
             args.decoder_model_dim, args.decoder_attention_heads,
             dropout=args.attention_dropout,
@@ -143,8 +421,6 @@ class Reduction(nn.Module):
                 need_weights=False,
                 attn_mask=None,
             )
-            x = F.dropout(x, p=self.dropout, training=self.training)
-            x = x + g
             x = x.squeeze(1)
             x = self.maybe_layer_norm(self.layer_norm, x, after=True)
             return x
